81ac7fe0b07085fa6af5cd9f3c92c6fe91049316
Jon Higger <jonathan.higger@gmail.com>
2024-09-16 14:21:09 -0600

    fixed style

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "bun:test";
+import { describe, it, expect } from "bun:test";
 import type { Student } from "./types";
 import { createGroups, getGroupOverlap } from "./get-groups";
 
@@ -36,7 +36,7 @@ describe("createGroups", () => {
     },
   ];
 
-  test("should create groups of specified size", () => {
+  it("should create groups of specified size", () => {
     const groupSize = 2;
     const groups = createGroups(students, groupSize);
     expect(groups.length).toBe(Math.ceil(students.length / groupSize));
@@ -45,7 +45,7 @@ describe("createGroups", () => {
     });
   });
 
-  test("should create groups with default size of 4", () => {
+  it("should create groups with default size of 4", () => {
     const groups = createGroups(students);
     expect(groups.length).toBe(Math.ceil(students.length / 4));
     groups.forEach((group) => {
@@ -53,26 +53,26 @@ describe("createGroups", () => {
     });
   });
 
-  test("should handle case with no students", () => {
+  it("should handle case with no students", () => {
     const groups = createGroups([]);
     expect(groups).toEqual([]);
   });
 
-  test("should handle case with group size larger than number of students", () => {
+  it("should handle case with group size larger than number of students", () => {
     const groupSize = 10;
     const groups = createGroups(students, groupSize);
     expect(groups.length).toBe(1);
     expect(groups[0].length).toBe(students.length);
   });
 
-  test("should group students by technical skill level", () => {
+  it("should group students by technical skill level", () => {
     const groups = createGroups(students, 2);
     expect(groups[0][0].technicalSkillLevel).toBeGreaterThanOrEqual(
       groups[0][1].technicalSkillLevel
     );
   });
 
-  test("should group students with maximum overlap", () => {
+  it("should group students with maximum overlap", () => {
     const groups = createGroups(students, 2);
     groups.forEach((group) => {
       if (group.length > 1) {
